3169d02eb766ef541c9ae4655fe711824e6dbe05
ElliotEckholm <ellioteckholm@gmail.com>
2023-02-07 16:39:26 -0700

    Update noCuts.py

--- noCuts.py
+++ noCuts.py
@@ -39,11 +39,6 @@ scale_factor = YTArray([(1 / (1 + redshift ))])
 
 lambda_a = (lambda_0) / ( (matter_0/(scale_factor**3)) + lambda_0)
 
-# print("----lambda_a------")
-# print(lambda_a)
-# print("----------")
-
-
 if (smoothing == 0.5):
     if (redshift == 0):
         filepath = filepath_z_0_s_0
@@ -61,96 +56,27 @@ data = dict(matter_density = (simulation_data*density_conv, "g/cm**3"),particle_
 bbox = np.array([[0, sim_dim], [0, sim_dim], [0, sim_dim]])
 
 #load array into yt dataset
-# ds = yt.load_uniform_grid(data, simulation_data.shape, length_unit=(voxel, "Mpc"), mass_unit=(1.0,"Msun"),bbox=bbox, nprocs=box_size)
 ds = yt.load_uniform_grid(data, simulation_data.shape, length_unit="Mpc", bbox=bbox, nprocs=box_size)
-# ad = ds.all_data()
 
 def matter_density_field(field, data):
     matter_a = (matter_0 / (scale_factor**3)) / ( (matter_0 / (scale_factor**3)) + lambda_0)
-    # print("----matter_a------")
-    # print(matter_a)
-    # print(np.average(data['matter_density'] ))
-    # print("----------")
-
     matter_a = data['matter_density'] * (matter_0 / lambda_0 )
-
-    # return (data['matter_density']    / lambda_a).in_units("g/cm**3")
     return ( matter_a    / lambda_a).in_units("g/cm**3")
 
 
 
 def dark_energy_density_field(field, data):
     yt_lambda_a = yt.YTArray(lambda_a,"g/cm**3")
-    # print("-----yt lambda_a-----")
-    # print(yt_lambda_a)
-    # print("----------")
-    # yt_lambda_a = YTArray([lambda_a]).in_units("g/cm**3")
     return ((yt_lambda_a[0]).in_units("g/cm**3"))
 
 
-# print("----matter_0 / lambda_0 ------")
-# print(matter_0 / lambda_0)
-# print("----------")
-
 
 ds.add_field(('gas','matter_density_field'), function=matter_density_field, units="g/cm**3")
-
 ds.add_field(('gas','dark_energy_density_field'), function=dark_energy_density_field, units="g/cm**3")
-
-
-
 L = [1,1,0] # vector normal to cutting plane
 
-
-
 for index in range(len(density_ratios)):
-
-    # fig = plt.figure()
-    # grid = AxesGrid(fig, (0.075,0.075,0.85,0.85,0.95),
-    #                 nrows_ncols = (2, 2),
-    #                 axes_pad = 1.0,
-    #                 label_mode = "1",
-    #                 share_all = True,
-    #                 cbar_location="right",
-    #                 cbar_mode="each",
-    #                 cbar_size="3%",
-    #                 cbar_pad="10%")
-
     slc = yt.SlicePlot(ds,'z',  ['matter_density_field'],origin="native",center=[0,0,0],width=(369,'Mpc'))
-
-
-
-    #
-    #
-    # plot = slc.plots["matter_density_field"]
-    # # plot.figure = fig
-    # # plot.axes = grid[index].axes
-    # # plot.cax = grid.cbar_axes[index]
-    # colorbar = plot.cb
-    # #
-    # # # colorbar.set_ticks([density_ratios[0]*density_conv,density_ratios[1]*density_conv,density_ratios[2]*density_conv,density_ratios[3]*density_conv,density_ratios[4]*density_conv])
-    # # # colorbar.set_ticks([density_ratios[index]*density_conv])
-    # # # colorbar.set_ticklabels(['$420$'])
-    # #
-    # colorbar.set_ticks([1e-28])
-    # colorbar.set_ticklabels(['$10^{-28}$'])  #
-
-    # slc.set_zlim('matter_density_field', density_ratios[index]*density_conv, (density_ratios[index]+0.1)*density_conv) #4.1*density_conv)
-    # slc.set_cmap('matter_density_field', "Blues")
-    # slc.hide_colorbar()
-
-    # This is the plot object.
-
-
-
-
-    #
-    # cbar.set_ticks([5e-29])
-    # cbar.set_ticklabels(['$420$'])
-    # slc.annotate_grids(cmap=None)
-    # slc6.set_unit('z', 'Mpc/h')
-
-
     if (smoothing == 0.5):
         if (redshift == 0):
             slc.annotate_title('No Cuts z = 0')
@@ -165,52 +91,3 @@ for index in range(len(density_ratios)):
             # slc._setup_plots()
             slc.save('z1/'+filepath+str(density_ratios[index])+"z=1")
 
-
-#
-# slc = yt.SlicePlot(ds,'z',  ['matter_density_field'],origin="native",center=[0,0,0],width=(369,'Mpc'))
-#
-# cm = yt.visualization.color_maps.make_colormap([('red', 10), ('orange', 10), ('yellow', 10), ('green', 10), ('blue', 10)], name="steps",interpolate=False)
-# slc.set_zlim('matter_density_field', density_ratios[0]*density_conv, (density_ratios[4])*density_conv) #4.1*density_conv)
-# slc.set_cmap('matter_density_field', cm)
-#
-# slc.hide_colorbar()
-# # slc.annotate_grids(cmap=None)
-# # slc6.set_unit('z', 'Mpc/h')
-# if (smoothing == 0.5):
-#     if (redshift == 0):
-#         slc.annotate_title('DM/DE = '+'multicolor'+' z = 0')
-#         slc.save('z0_de/'+filepath+'multicolor'+"z=0"+'custom')
-#     elif (redshift == 0.5):
-#         slc.annotate_title('DM/DE = '+'multicolor'+' z = 0.5')
-#         slc.save('z5_de/'+filepath+'multicolor'+"z=05"+'custom')
-#     elif (redshift == 1):
-#         slc.annotate_title('DM/DE = '+str(density_ratios[index])+' z = 1')
-#         slc.save('z1_de/'+filepath+'multicolor'+"z=1"+'custom')
-#
-
-
-
-
-
-
-# slc = yt.SlicePlot(ds,'z',  ['matter_density_field'],origin="native",center=[0,0,0],width=(369,'Mpc'))
-#
-#
-# slc.set_zlim('matter_density_field', density_ratios[0]*density_conv, (density_ratios[4])*density_conv) #4.1*density_conv)
-# slc.set_cmap('matter_density_field', "Rainbow")
-#
-# slc.hide_colorbar()
-# slc.annotate_grids(cmap=None)
-# slc.annotate_contour("matter_density_field")
-# # slc6.set_unit('z', 'Mpc/h')
-# if (smoothing == 0.5):
-#     if (redshift == 0):
-#         slc.annotate_title('DM/DE = '+'multicolor'+' z = 0')
-#         slc.save('z0_de/'+filepath+'multicolor'+"z=0"+'rainbow')
-#     elif (redshift == 0.5):
-#         slc.annotate_title('DM/DE = '+'multicolor'+' z = 0.5')
-#         slc.save('z5_de/'+filepath+'multicolor'+"z=05"+'rainbow')
-#     elif (redshift == 1):
-#         slc.annotate_title('DM/DE = '+str(density_ratios[index])+' z = 1')
-#         slc.save('z1_de/'+filepath+'multicolor'+"z=1"+'rainbow')
-#
